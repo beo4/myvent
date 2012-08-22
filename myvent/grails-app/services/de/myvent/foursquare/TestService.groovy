4ae36185f4dedf3f27e792b4c7f8eba0b9a81f27
beo4 <behounek@W1IT10.kathrein.de>
2012-08-22 15:26:29 +0200

    rrrr

---
+++ myvent/grails-app/services/de/myvent/foursquare/TestService.groovy
@@ -0,0 +1,12 @@
+package de.myvent.foursquare
+
+class TestService {
+	def grailsApplication
+	
+			def readFoo() {
+					assert grailsApplication.config
+					def paramter = grailsApplication.config.plugins.springsocial.foursquare.clientId
+					return parameter
+			}
+   
+}
