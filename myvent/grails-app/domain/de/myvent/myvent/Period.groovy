623537efc912f19f93cc4022f4c0f8f0e3b1444a
beo4 <c.behounek+github@googlemail.com>
2012-09-05 17:45:06 +0200

    start Period

--- myvent/grails-app/domain/de/myvent/myvent/Period.groovy
+++ myvent/grails-app/domain/de/myvent/myvent/Period.groovy
@@ -6,6 +6,10 @@ import org.joda.time.LocalDateTime;
 class Period {
 
     static constraints = {
+		start (validator: { val, obj ->
+        val?.isAfter(new LocalDateTime())})
+		stop(validator: { val, obj ->
+        val?.isAfter(obj.start)})
     }
 	
 	LocalDateTime start
